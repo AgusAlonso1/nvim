c9a86595e6ec7dac5aab851ba22062d0f9a42da1
AgusAlonso1 <agusalonso@itba.edu.ar>
2024-10-08 12:31:28 -0300

    Changes in colorscheme and lualine configuration.

--- lua/agus/plugins/lualine.lua
+++ lua/agus/plugins/lualine.lua
@@ -6,6 +6,50 @@ return {
 
         local custom_horizon = require "lualine.themes.horizon"
 
+        local custom_theme = {
+            normal = {
+                a = { fg = '#212539', bg = '#7aa2f7', gui = 'bold' },
+                b = { fg = '#6b8cd4', bg = '#3b4261' },
+                c = { fg = '#a4abd0', bg = '#1f2335' },
+            },
+            insert = {
+                a = { fg = '#212539', bg = '#98c668', gui = 'bold' },
+                b = { fg = '#98c668', bg = '#3b4261' },
+                c = { fg = '#a4abd0', bg = '#1f2335' },
+            },
+            visual = {
+                a = { fg = '#212539', bg = '#ad0afd', gui = 'bold' },
+                b = { fg = '#ad0afd', bg = '#3b4261' },
+                c = { fg = '#a4abd0', bg = '#1f2335' },
+            },
+            command = {
+                a = { fg = '#212539', bg = '#e07022', gui = 'bold' },
+                b = { fg = '#e07022', bg = '#3b4261' },
+                c = { fg = '#a4abd0', bg = '#1f2335' },
+            },
+            replace = {
+                a = { fg = '#212539', bg = '#ed1c24', gui = 'bold' },
+                b = { fg = '#ed1c24', bg = '#3b4261' },
+                c = { fg = '#a4abd0', bg = '#1f2335' },
+            },
+        }
+
+        local custom_config = {
+            options = {
+                theme = custom_theme;
+                section_separators = { left = '', right = ''},
+                component_separators = { left = '|', right = '|'},
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch' },
+                lualine_c = { 'diagnostics', 'filename' },
+                lualine_x = { 'diff', 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+        }
+
         -- Normal Mode
         custom_horizon.normal.a.bg = '#dbfb0c'
         custom_horizon.normal.a.fg = '#181922'
@@ -51,11 +95,7 @@ return {
         custom_horizon.replace.c.bg = '#021723'
         custom_horizon.replace.c.fg = '#ffffff'
 
-        lualine.setup({
-            options = {
-                theme = custom_horizon,
-            },
-        })
+        lualine.setup(custom_config)
     end,
 
 }
